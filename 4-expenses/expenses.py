e8ba53417b145abbf28b48b90f06dc958d76668c
yukolokolcev <urbas2303@yandex.ru>
2026-04-14 20:05:39 +0300

    fix

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,5 +1,5 @@
 expenses = [100, 43, 96, 12, 5000, 6, 2]
 
-expenses_tuple = (min(expenses), max(expenses), sum(expenses) / len(expenses))
+expenses_tuple = (min(expenses), max(expenses), sum(expenses))
 
 print(expenses_tuple)
